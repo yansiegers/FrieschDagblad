773c46f8b5a5129fbd5ca15d0e0f767b44870b4a
Yan Siegers <email@yansiegers.nl>
2020-06-04 12:47:56 +0200

    Add FD colors and default link styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fdblue: '#00a4e8',
+        fdwhite: '#ffffff',
+        fdgray: {
+          default: '#ededed',
+          darker: '#c4c4c4'
+        },
+        fdblack: '#000000'
+      }
+    },
   },
   variants: {},
   plugins: [],
