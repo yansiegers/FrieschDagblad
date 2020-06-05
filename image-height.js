efbda64e1ce2872e29369bc459ee033913b91632
Yan Siegers <email@yansiegers.nl>
2020-06-05 20:31:02 +0200

    Cleanup

--- image-height.js
+++ image-height.js
@@ -3,29 +3,12 @@ contentElements = document.getElementsByClassName("content-height");
 
 for (var i = 0; i < imageElements.length; i++) {
   var image = imageElements[i];
-  console.log(image);
-
   var imageParent = image.parentElement;
-  console.log(imageParent);
-
   var content = contentElements[i];
-  console.log(content);
-
-  console.log("---------------------------------------")
 
   var fullWidth = imageParent.offsetWidth;
-  console.log(fullWidth);
-
-  // var fullHeight = imageParent.offsetHeight;
-  // console.log(fullHeight);
-
   var contentHeight = content.offsetHeight;
-  console.log(contentHeight);
-
-  // var newImageHeight = fullHeight - contentHeight;
   var newImageHeight = fullWidth - contentHeight;
-  console.log(newImageHeight);
 
   image.style.height = newImageHeight;
-  // image.offsetHeight = newImageHeight;
 }
