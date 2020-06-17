eb797052127cb3598f24b4b76e048e36f056bad4
Yan Siegers <email@yansiegers.nl>
2020-06-17 15:55:41 +0200

    Add js tab switcher

---
+++ public/js/tabs.js
@@ -0,0 +1,32 @@
+function showTabMostRead() {
+  console.log('showlistMostRead');
+
+  var listJustIn = document.getElementById("list-just-in");
+  var listMostRead = document.getElementById("list-most-read");
+  var tabJustIn = document.getElementById("tab-just-in");
+  var tabMostRead = document.getElementById("tab-most-read");
+
+  if (listMostRead.classList.contains("hidden")) {
+    listMostRead.classList.remove("hidden");
+    tabMostRead.classList.remove("hidden");
+    listJustIn.classList.add("hidden");
+    tabJustIn.classList.add("hidden");
+  }
+
+}
+
+function showTabJustIn() {
+  console.log('showlistJustIn');
+
+  var listJustIn = document.getElementById("list-just-in");
+  var listMostRead = document.getElementById("list-most-read");
+  var tabJustIn = document.getElementById("tab-just-in");
+  var tabMostRead = document.getElementById("tab-most-read");
+
+  if (listJustIn.classList.contains("hidden")) {
+    tabJustIn.classList.remove("hidden");
+    listJustIn.classList.remove("hidden");
+    tabMostRead.classList.add("hidden");
+    listMostRead.classList.add("hidden");
+  }
+}
