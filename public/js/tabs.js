6fbccb8c08fe303ad417b5526df1d18c1c95c665
Yan Siegers <email@yansiegers.nl>
2020-06-17 16:01:05 +0200

    Convert functions to toggle

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -1,23 +1,4 @@
-function showTabMostRead() {
-  console.log('showlistMostRead');
-
-  var listJustIn = document.getElementById("list-just-in");
-  var listMostRead = document.getElementById("list-most-read");
-  var tabJustIn = document.getElementById("tab-just-in");
-  var tabMostRead = document.getElementById("tab-most-read");
-
-  if (listMostRead.classList.contains("hidden")) {
-    listMostRead.classList.remove("hidden");
-    tabMostRead.classList.remove("hidden");
-    listJustIn.classList.add("hidden");
-    tabJustIn.classList.add("hidden");
-  }
-
-}
-
-function showTabJustIn() {
-  console.log('showlistJustIn');
-
+function toggleTabs() {
   var listJustIn = document.getElementById("list-just-in");
   var listMostRead = document.getElementById("list-most-read");
   var tabJustIn = document.getElementById("tab-just-in");
@@ -28,5 +9,10 @@ function showTabJustIn() {
     listJustIn.classList.remove("hidden");
     tabMostRead.classList.add("hidden");
     listMostRead.classList.add("hidden");
+  } else if (listMostRead.classList.contains("hidden")) {
+    listMostRead.classList.remove("hidden");
+    tabMostRead.classList.remove("hidden");
+    listJustIn.classList.add("hidden");
+    tabJustIn.classList.add("hidden");
   }
 }
