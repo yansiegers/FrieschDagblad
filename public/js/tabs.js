64d95a44e320487717a05f9a122a425ed727b625
Yan Siegers <email@yansiegers.nl>
2020-06-17 19:00:40 +0200

    Code optimizations

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -1,26 +1,28 @@
+var listMostRead = document.getElementById("list-most-read");
+var listJustIn = document.getElementById("list-just-in");
+var tabMostRead = document.getElementById("tab-most-read");
+var tabJustIn = document.getElementById("tab-just-in");
+
 function toggleTabs() {
-  var listJustIn = document.getElementById("list-just-in");
-  var listMostRead = document.getElementById("list-most-read");
-  var tabs = document.getElementById("tabs");
 
   if (listJustIn.classList.contains("hidden")) {
 
     // hide most-read
     listMostRead.classList.add("hidden");
-    tabs.children[0].classList.add("text-fdwhite", "bg-fdblue", "shadow-inner");
+    tabMostRead.classList.add("text-fdwhite", "bg-fdblue", "shadow-inner");
 
     // show just-in
     listJustIn.classList.remove("hidden");
-    tabs.children[1].classList.remove("text-fdwhite", "bg-fdblue", "shadow-inner");
+    tabJustIn.classList.remove("text-fdwhite", "bg-fdblue", "shadow-inner");
 
   } else if (listMostRead.classList.contains("hidden")) {
     // show most-read    
     listMostRead.classList.remove("hidden");
-    tabs.children[0].classList.remove("text-fdwhite", "bg-fdblue", "shadow-inner");
+    tabMostRead.classList.remove("text-fdwhite", "bg-fdblue", "shadow-inner");
 
     // hide just-in
     listJustIn.classList.add("hidden");
-    tabs.children[1].classList.add("text-fdwhite", "bg-fdblue", "shadow-inner");
+    tabJustIn.classList.add("text-fdwhite", "bg-fdblue", "shadow-inner");
 
   }
 }
